deb19a51622f84c649b47e1bd6564021b7cdddba
rails --v <ruby -v>
2023-07-22 14:45:43 +0300

    Setup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  resources :categories do 
+    resources :payments
+  end
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get '/logout', to: 'devise/sessions#destroy'
+  end  
+  root to: 'users#index'
 end
